41fe50519b2ca3f80d5e66dcc14a2ba7124e4c0d
Dharani0196python <112925299+Dharani0196python@users.noreply.github.com>
2022-10-04 10:06:38 +0530

    Add files via upload

---
+++ upper & lower case.py
@@ -0,0 +1,18 @@
+# Python program to count uppercase and lowercase characters
+
+# take input
+string = input('Enter any string: ')
+
+upper, lower = 0, 0
+for i in string:
+    #count lowercase characters
+    if(i.islower()):
+        lower = lower + 1
+    #count uppercase characters
+    elif(i.isupper()):
+        upper = upper + 1
+
+# print number of lowercase characters
+print('Lowercase characters:',lower)
+# print number of uppercase characters
+print('Uppercase characters:',upper)
